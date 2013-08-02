453850c2207c58a0345e49b7f07ad89a5eaec6c5
pixlepix <adlytempleton@gmail.com>
2013-08-02 14:19:10 -0400

    AUTO

--- src/minecraft/pixlepix/missioncontrol/common/tile/OutputInventoryLinkTileEntity.java
+++ src/minecraft/pixlepix/missioncontrol/common/tile/OutputInventoryLinkTileEntity.java
@@ -1,11 +1,26 @@
 package pixlepix.missioncontrol.common.tile;
 
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.ForgeSubscribe;
+import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
 
 public class OutputInventoryLinkTileEntity extends TileEntity {
 	public String player;
 	
 	public OutputInventoryLinkTileEntity(){
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+	public void dropItem(ItemStack item){
 		
+	}
+	@ForgeSubscribe
+	public void onPickup(EntityItemPickupEvent e){
+		if(e.entityPlayer.username.equals(player)){
+			ItemStack stack=e.item.getEntityItem();
+			e.item.setDead();
+			e.setCanceled(true);
+		}
 	}
 }
