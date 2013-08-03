d45ffb65f27da4720fe5ac134f5ab2f158be4876
pixlepix <adlytempleton@gmail.com>
2013-08-03 16:09:12 -0400

    AUTO

---
+++ src/minecraft/pixlepix/missioncontrol/common/helper/HeadSpecialRenderer.java
@@ -0,0 +1,18 @@
+package pixlepix.missioncontrol.common.helper;
+
+import net.minecraft.client.renderer.RenderBlocks;
+import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.tileentity.TileEntity;
+
+public class HeadSpecialRenderer extends TileEntitySpecialRenderer {
+
+	
+	
+	 private final RenderBlocks renderBlocks = new RenderBlocks();
+
+	    @Override
+	    public void renderTileEntityAt(TileEntity tileEntity, double x, double y, double z, float var8)
+	    {
+	    	
+	    }
+}
