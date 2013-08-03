7d5010bc5a1fefa7299fb0fde67bb773ce987f53
pixlepix <adlytempleton@gmail.com>
2013-08-03 15:15:45 -0400

    AUTO

--- src/minecraft/pixlepix/missioncontrol/common/helper/Config.java
+++ src/minecraft/pixlepix/missioncontrol/common/helper/Config.java
@@ -1,5 +1,13 @@
 package pixlepix.missioncontrol.common.helper;
 
+import net.minecraftforge.event.ForgeSubscribe;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
 public class Config {
 
+	
+	@ForgeSubscribe
+	public static void configure(FMLPreInitializationEvent event){
+		
+	}
 }
