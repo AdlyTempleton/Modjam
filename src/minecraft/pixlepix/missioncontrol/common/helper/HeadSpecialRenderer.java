5db172c7cc3836c1f00db2ac0d96212bd46bceab
pixlepix <adlytempleton@gmail.com>
2013-08-03 16:11:12 -0400

    AUTO

--- src/minecraft/pixlepix/missioncontrol/common/helper/HeadSpecialRenderer.java
+++ src/minecraft/pixlepix/missioncontrol/common/helper/HeadSpecialRenderer.java
@@ -3,6 +3,10 @@ package pixlepix.missioncontrol.common.helper;
 import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.tileentity.TileEntity;
+import pixlepix.missioncontrol.common.tile.DeathInventoryLinkTileEntity;
+import pixlepix.missioncontrol.common.tile.InputInventoryLinkTileEntity;
+import pixlepix.missioncontrol.common.tile.InventoryLinkTileEntity;
+import pixlepix.missioncontrol.common.tile.OutputInventoryLinkTileEntity;
 
 public class HeadSpecialRenderer extends TileEntitySpecialRenderer {
 
@@ -10,6 +14,18 @@ public class HeadSpecialRenderer extends TileEntitySpecialRenderer {
 	
 	 private final RenderBlocks renderBlocks = new RenderBlocks();
 	 	public String getPlayerName(TileEntity entity){
+	 		if(entity instanceof InventoryLinkTileEntity){
+	 			return ((InventoryLinkTileEntity)entity).player;
+	 		}
+	 		if(entity instanceof InputInventoryLinkTileEntity){
+	 			return ((InputInventoryLinkTileEntity)entity).player;
+	 		}
+	 		if(entity instanceof OutputInventoryLinkTileEntity){
+	 			return ((OutputInventoryLinkTileEntity)entity).player;
+	 		}
+	 		if(entity instanceof DeathInventoryLinkTileEntity){
+	 			return ((DeathInventoryLinkTileEntity)entity).player;
+	 		}
 	 		return null;
 	 	}
 	    @Override
