6225d4b9650823d6f53d2ad1cf2ebbbe43e511b9
pixlepix <adlytempleton@gmail.com>
2013-08-03 15:16:45 -0400

    AUTO

--- src/minecraft/pixlepix/missioncontrol/common/helper/Config.java
+++ src/minecraft/pixlepix/missioncontrol/common/helper/Config.java
@@ -6,6 +6,22 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 public class Config {
 
 	
+	public static int inventoryLink=1215;
+
+	public static int SelectiveInventoryLink=1216;
+
+	public static int inventoryLink=1215;
+
+	public static int inventoryLink=1215;
+
+	public static int inventoryLink=1215;
+
+	public static int inventoryLink=1215;
+
+	public static int inventoryLink=1215;
+
+	public static int inventoryLink=1215;
+	
 	@ForgeSubscribe
 	public static void configure(FMLPreInitializationEvent event){
 		
