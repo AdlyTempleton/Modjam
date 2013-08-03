970322b8c31cb5a48ee987ba6de49ead8c7080c6
pixlepix <adlytempleton@gmail.com>
2013-08-03 10:11:14 -0400

    AUTO

--- src/minecraft/pixlepix/missioncontrol/common/tile/InventoryLinkTileEntity.java
+++ src/minecraft/pixlepix/missioncontrol/common/tile/InventoryLinkTileEntity.java
@@ -15,7 +15,10 @@ public class InventoryLinkTileEntity extends TileEntity implements IInventory {
 	public String player;
 	@Override
 	public int getSizeInventory() {
-		
+		if(worldObj.isBlockIndirectlyGettingPowered(xCoord, yCoord, zCoord)){
+			System.out.println("Redstone Halt");
+			return 0;
+		}
 		return 31;
 	}
 
