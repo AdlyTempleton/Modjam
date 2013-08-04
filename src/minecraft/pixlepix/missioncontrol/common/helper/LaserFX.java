74c06ed3282f43fcf6767a0cb9a05ae07587d208
pixlepix <adlytempleton@gmail.com>
2013-08-04 09:46:23 -0400

    AUTO

--- src/minecraft/pixlepix/missioncontrol/common/helper/LaserFX.java
+++ src/minecraft/pixlepix/missioncontrol/common/helper/LaserFX.java
@@ -31,7 +31,8 @@ public class LaserFX extends EntityFX {
     private String texture;
     
     
-    public float calculateDistance(){
+    public float calculateDistance(float x1,float y1,float z1,float x2,float y2, float z2){
+    	return Math.sqrt(Math.pow(x1-x2, 2)+(Math.pow(y1-y2, 2)+(Math.pow(z1-z2, 2));
     	
     }
     
