a7308c69b908375e1b5f51c24828631de2fd95bb
pixlepix <adlytempleton@gmail.com>
2013-08-03 22:44:56 -0400

    AUTO

--- src/minecraft/pixlepix/missioncontrol/common/MissionControl.java
+++ src/minecraft/pixlepix/missioncontrol/common/MissionControl.java
@@ -78,7 +78,7 @@ public class MissionControl {
 		GameRegistry.addShapelessRecipe(new ItemStack(proximityInventoryLink), new ItemStack(inventoryLink),new ItemStack(Block.pressurePlateStone));
 		GameRegistry.addShapelessRecipe(new ItemStack(enderInventoryLink), new ItemStack(inventoryLink),new ItemStack(Item.enderPearl));
 		GameRegistry.addShapelessRecipe(new ItemStack(deathInventoryLink), new ItemStack(inventoryLink),new ItemStack(Item.bone));
-		GameRegistry.addShapelessRecipe(new ItemStack(selectiveInventoryLink), new ItemStack(inventoryLink),new ItemStack(Block.glass));
+		GameRegistry.addShapelessRecipe(new ItemStack(outputInventoryLink), new ItemStack(inventoryLink),new ItemStack(Block.blockHopper)));
 		GameRegistry.addShapelessRecipe(new ItemStack(selectiveInventoryLink), new ItemStack(inventoryLink),new ItemStack(Block.glass));
 		
 	}
