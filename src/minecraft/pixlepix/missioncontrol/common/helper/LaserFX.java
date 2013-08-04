309be5fafe190e0e85305e9ba1e1b53f482645a3
pixlepix <adlytempleton@gmail.com>
2013-08-04 09:37:21 -0400

    AUTO

--- src/minecraft/pixlepix/missioncontrol/common/helper/LaserFX.java
+++ src/minecraft/pixlepix/missioncontrol/common/helper/LaserFX.java
@@ -20,7 +20,46 @@ public class LaserFX {
     private int rotationSpeed = 20;
     private float prevSize = 0.0F;
     private float beamD = 0.08f;
-	
+    
+    public LaserFX(World world, Vector3 start, Vector3 end, Color color, String texture, int age)
+    {
+        super(world, start.x, start.y, start.z, 0.0D, 0.0D, 0.0D);
+
+        this.setRGB(color.getRed(), color.getGreen(), color.getBlue());
+
+        this.texture = texture;
+
+        this.setSize(0.02F, 0.02F);
+        this.noClip = true;
+        this.motionX = 0.0D;
+        this.motionY = 0.0D;
+        this.motionZ = 0.0D;
+        this.target = end;
+        float xd = (float) (this.posX - this.target.x);
+        float yd = (float) (this.posY - this.target.y);
+        float zd = (float) (this.posZ - this.target.z);
+        this.length = (float) new Vector3(this).distanceTo(this.target);
+        double var7 = MathHelper.sqrt_double(xd * xd + zd * zd);
+        this.rotYaw = ((float) (Math.atan2(xd, zd) * 180.0D / 3.141592653589793D));
+        this.rotPitch = ((float) (Math.atan2(yd, var7) * 180.0D / 3.141592653589793D));
+        this.prevYaw = this.rotYaw;
+        this.prevPitch = this.rotPitch;
+
+        this.particleMaxAge = age;
+
+        /** Sets the particle age based on distance. */
+        EntityLivingBase renderentity = Minecraft.getMinecraft().renderViewEntity;
+        int visibleDistance = 50;
+
+        if (!Minecraft.getMinecraft().gameSettings.fancyGraphics)
+        {
+            visibleDistance = 25;
+        }
+        if (renderentity.getDistance(this.posX, this.posY, this.posZ) > visibleDistance)
+        {
+            this.particleMaxAge = 0;
+        }
+    }
 	
 	
 }
