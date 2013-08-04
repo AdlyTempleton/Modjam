fe80c10ab04f42125f77f88d1b436aa89e60803a
pixlepix <adlytempleton@gmail.com>
2013-08-04 09:35:20 -0400

    AUTO

---
+++ src/minecraft/pixlepix/missioncontrol/common/helper/LaserFX.java
@@ -0,0 +1,24 @@
+package pixlepix.missioncontrol.common.helper;
+
+public class LaserFX {
+
+	double movX = 0.0D;
+    double movY = 0.0D;
+    double movZ = 0.0D;
+
+    private float length = 0.0F;
+    private float rotYaw = 0.0F;
+    private float rotPitch = 0.0F;
+    private float prevYaw = 0.0F;
+    private float prevPitch = 0.0F;
+    private Vector3 target = new Vector3();
+    private float endModifier = 1.0F;
+    private boolean reverse = false;
+    private boolean pulse = false;
+    private int rotationSpeed = 20;
+    private float prevSize = 0.0F;
+    private float beamD = 0.08f;
+	
+	
+	
+}
