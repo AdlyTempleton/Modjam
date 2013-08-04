682e41bd75c4c97179c9407586f66920b9bda7b7
pixlepix <adlytempleton@gmail.com>
2013-08-04 12:16:19 -0400

    AUTO

---
+++ src/minecraft/pixlepix/missioncontrol/common/helper/ItemPacket.java
@@ -0,0 +1,12 @@
+package pixlepix.missioncontrol.common.helper;
+
+import net.minecraft.item.Item;
+
+public class ItemPacket extends Item {
+
+	public ItemPacket(int par1) {
+		super(par1);
+		
+	}
+
+}
