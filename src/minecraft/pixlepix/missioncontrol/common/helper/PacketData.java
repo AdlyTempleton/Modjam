4283a863d99a19d8825ddd923a845c114e7f2f24
pixlepix <adlytempleton@gmail.com>
2013-08-04 12:29:22 -0400

    AUTO

--- src/minecraft/pixlepix/missioncontrol/common/helper/PacketData.java
+++ src/minecraft/pixlepix/missioncontrol/common/helper/PacketData.java
@@ -8,4 +8,14 @@ public class PacketData {
 	double endY;
 	double endZ;
 	float speed;
+	boolean headingDown;
+	
+	public PacketData(EntityItem entity, double endX, double endY, double endZ);
+	
+	
+	
+	public float calculateDistance(float x1,float y1,float z1,float x2,float y2, float z2){
+    	return (float)Math.sqrt(Math.pow(x1-x2, 2)+(Math.pow(y1-y2, 2))+(Math.pow(z1-z2, 2)));
+    	
+    }
 }
