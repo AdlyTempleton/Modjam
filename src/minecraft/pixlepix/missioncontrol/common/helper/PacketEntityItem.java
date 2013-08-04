554d4ddf54242254da62cb967db648a11d696a9a
pixlepix <adlytempleton@gmail.com>
2013-08-04 14:02:48 -0400

    AUTO

---
+++ src/minecraft/pixlepix/missioncontrol/common/helper/PacketEntityItem.java
@@ -0,0 +1,15 @@
+package pixlepix.missioncontrol.common.helper;
+
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+public class PacketEntityItem extends EntityItem {
+
+	public PacketEntityItem(World par1World, double par2, double par4,
+			double par6, ItemStack par8ItemStack) {
+		super(par1World, par2, par4, par6, par8ItemStack);
+		// TODO Auto-generated constructor stub
+	}
+
+}
