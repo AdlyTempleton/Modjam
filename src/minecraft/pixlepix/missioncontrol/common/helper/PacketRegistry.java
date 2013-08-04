310715a690c48244a1be65276a3b233453350941
pixlepix <adlytempleton@gmail.com>
2013-08-04 14:25:50 -0400

    AUTO

--- src/minecraft/pixlepix/missioncontrol/common/helper/PacketRegistry.java
+++ src/minecraft/pixlepix/missioncontrol/common/helper/PacketRegistry.java
@@ -26,10 +26,10 @@ public class PacketRegistry implements ITickHandler {
 
 			currentPacket.entity.delayBeforeCanPickup=1000;
 			
-			currentPacket.entity.motionX= (currentPacket.entity.posX-currentPacket.endX)/-160;
+			currentPacket.entity.motionX= (currentPacket.entity.posX-currentPacket.endX)/-40;
 
-			currentPacket.entity.motionY= (currentPacket.entity.posY-currentPacket.endY)/-160;
-			currentPacket.entity.motionZ= (currentPacket.entity.posZ-currentPacket.endZ)/-160;
+			currentPacket.entity.motionY= (currentPacket.entity.posY-currentPacket.endY)/-40;
+			currentPacket.entity.motionZ= (currentPacket.entity.posZ-currentPacket.endZ)/-40;
 			if(calculateDistance(currentPacket.endX,currentPacket.endY,currentPacket.endZ,currentPacket.entity.posX,currentPacket.entity.posY,currentPacket.entity.posZ)<2){
 				currentPacket.entity.setDead();
 				iter.remove();
